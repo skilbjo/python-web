54b826846b0e111f36d9938d9a47ed778897f489
John Skilbeck <john.skilbeck@gmail.com>
2013-10-27 00:22:00 -0700

    init

---
+++ email1.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+fromaddr = 'skilbjo@gmail.com'
+toaddr = 'john.skilbeck@gmail'
+text = 'test message sent from Python'
+username = 'skilbjo'
+password = 'school69'
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = 'Test'
+msg.attach(MIMEText(text))
+server = smtplib.SMTP('smtp.gmail.com:58')
+server.ehlo()
+server.startttls()
+server.ehlo()
+server.login(username,password)
+server.sendmail(fromaddr, toaddr, msg.as_string())
+server.quit()
